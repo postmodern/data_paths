17030f4c05ac7d1a8562f65eabf53aea5abdad6c
Postmodern <postmodern.mod3@gmail.com>
2011-04-19 18:18:44 -0700

    Added documentation.

--- lib/data_paths/data_paths.rb
+++ lib/data_paths/data_paths.rb
@@ -17,17 +17,36 @@ module DataPaths
     @data_paths ||= []
   end
 
+  #
+  # Registers a `data/` directory.
+  #
+  # @return [String]
+  #   The registered `data/` directory.
+  #
+  # @raise [RuntimeError]
+  #   The given path was not a directory.
+  #
+  # @since 0.3.0
+  #
   def DataPaths.register(path)
     path = File.expand_path(path)
 
     unless File.directory?(path)
-      raise(RuntimeError,"#{path.dump} must be a directory")
+      raise("#{path.dump} must be a directory")
     end
 
     paths << path unless paths.include?(path)
     return path
   end
 
+  #
+  # Unregisters a previously registered `data/` directory.
+  #
+  # @return [String]
+  #   The unregistered `data/` directory.
+  #
+  # @since 0.3.0
+  #
   def DataPaths.unregister!(path)
     paths.delete(File.expand_path(path))
   end
